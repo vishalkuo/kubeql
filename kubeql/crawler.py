b5b984fae08575530cda4bfa051a1e48009cb6d3
Vishal Kuo <vishalkuo@gmail.com>
2022-01-27 20:39:40 -0800

    add file path

--- kubeql/crawler.py
+++ kubeql/crawler.py
@@ -12,10 +12,11 @@ API_VERSION = "apiVersion"
 KUSTOMIZE_CMD = ["kubectl", "kustomize"]
 
 
-def _parse_yaml_str(raw_str: str) -> Iterator[dict[str, Any]]:
+def _parse_yaml_str(raw_str: str, file_path: str) -> Iterator[dict[str, Any]]:
     content = safe_load_all(raw_str)
     for obj in content:
         if API_VERSION in obj:
+            obj["_file_path"] = file_path
             yield obj
 
 
@@ -25,11 +26,11 @@ def crawl(dir_name: str, using_kustomization: bool = False) -> Iterator[dict[str
             full_path = os.path.join(dir, file)
             if file == KUSTOMIZATION_YAML and using_kustomization:
                 output = subprocess.run(KUSTOMIZE_CMD + [dir], capture_output=True)
-                yield from _parse_yaml_str(output.stdout)
+                yield from _parse_yaml_str(output.stdout, full_path)
             elif file.endswith(".yaml") and not using_kustomization:
                 with open(full_path) as f:
                     raw_yaml = f.read()
-                yield from _parse_yaml_str(raw_yaml)
+                yield from _parse_yaml_str(raw_yaml, full_path)
 
 
 def load_into_db(db: Database, dir: str, using_kustomization: bool = False) -> None:
